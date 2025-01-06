06e2da9a9e0163fd80db754a9ad60a7e05e9e443
JekaFIT <elatyntsev@gmail.com>
2025-01-06 11:23:21 +0300

    06/01/25

---
+++ Module_7_4.py
@@ -0,0 +1,29 @@
+team1_num = 5
+result = "В команде Мастера кода участников: %d !" % team1_num
+print(result)
+
+
+team1_num = 5
+team2_num = 6
+result = "Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)
+print(result)
+
+
+score_2 = 42
+result = "Команда Волшебники данных решила задач: {} !".format(score_2)
+print(result)
+
+team1_time = 18015.2
+result = "Волшебники данных решили задачи за {}c!".format(team1_time)
+print(result)
+
+score_1 = 40
+score_2 = 42
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+challenge_result = 'Мастера кода'
+print(f'Результат битвы: победа команды {challenge_result}!')
+
+tasks_tota = 82
+time_avg = 350.4
+print(f'Сегодня было решено {tasks_tota} задач, в среднем по {time_avg} секунды на задачу!')
